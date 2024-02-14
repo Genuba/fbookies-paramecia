476b4076bae49f5cd1f4b9ddcf6544e357a9b82b
Genuba <daalpiva@gmail.com>
2024-02-14 13:39:38 -0500

    update transformation

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import difflib
+
 from pymongo_get_database import get_database
 
 db = get_database()
@@ -5,5 +7,45 @@ betsByDate = db["betsByDate"]
 
 docData = betsByDate.find({})
 
-for x in docData:
-    print(x)
+
+def getBetsByTeamName(n):
+    betBookieName_dic = {}
+    for bet in n["bets"]:
+        key = bet["bookie"]
+
+        if not key in betBookieName_dic:
+            betBookieName_dic[key] = {}
+
+        index = (
+            bet["teamA"]["name"]
+            + " vs "
+            + bet["teamB"]["name"]
+            + "----"
+            + bet["teamB"]["name"]
+            + " vs "
+            + bet["teamA"]["name"]
+        )
+
+        betBookieName_dic[key][index] = bet
+    return betBookieName_dic
+
+
+def groupBetsByTeamMatch(n):
+    print(n)
+
+
+#    keysList = list(n.keys())
+#    print(keysList)
+#
+#    for betMatch in n.keys():
+#        keysList.remove(betMatch)
+#        for key in keysList:
+#            if len(keysList):
+#                score = difflib.SequenceMatcher(None, betMatch, key).ratio()
+#                print("score for: " + betMatch + " ----- " + key + " = " + str(score))
+#
+
+for rowBet in docData:
+    print("------------- " + rowBet["timestamp"] + " ---------------")
+    betBookieName_dic = getBetsByTeamName(rowBet)
+    groupBetsByTeamMatch(betBookieName_dic)
